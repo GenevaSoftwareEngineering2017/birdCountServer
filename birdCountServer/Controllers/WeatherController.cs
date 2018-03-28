ec89c29fe0441e7e3ffcd3b0eefc138279bf8108
Jacob Girvin <jacobgirvin@yahoo.com>
2018-03-27 21:27:13 -0400

    Weather API Finished

--- birdCountServer/Controllers/WeatherController.cs
+++ birdCountServer/Controllers/WeatherController.cs
@@ -6,28 +6,55 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Data.SqlClient;
 using System.Text;
+using birdCountServer.Domain;
 
 
-namespace birdCountServer.Controllers
+namespace WeatherServer.Controllers
 {
+    [RoutePrefix("api/Weather")]
     public class WeatherController : ApiController
     {
-        public string Get()
+        [Route("getall")]
+        public List<Weather> GetAll()
         {
-            var retWeather = string.Empty;
+            return RetrieveWeatherFromDB(string.Empty);
+        }
+
+        [Route("getWeatherbyid/{id}")]
+        public Weather GetWeather(int id)
+        {
+            return RetrieveWeatherFromDB(string.Format("WeatherID = {0}", id)).FirstOrDefault();
+        }
+
+        //[Route("getbirdbyname/{name}")]
+        //public Weather GetWeather(string name)
+        //{
+        //    return RetrieveWeatherFromDB(string.Format("BirdName = '{0}'", name)).FirstOrDefault();
+        //}
+
+        public List<Weather> RetrieveWeatherFromDB(string whereClause)
+        {
+            var retWeather = new List<Weather>();
             try
             {
                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                builder.DataSource = "birdcountapp.database.windows.net";
-                builder.UserID = "User";
-                builder.Password = "Password1";
-                builder.InitialCatalog = "birdCountApp";
+                builder.DataSource = "birdcount.database.windows.net";
+                builder.UserID = "jmgirvin";
+                builder.Password = "Devan2/12/16";
+                builder.InitialCatalog = "BirdCountDatabase";
 
                 using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
                 {
                     connection.Open();
                     StringBuilder sb = new StringBuilder();
-                    sb.Append("select * from Weather");
+
+                    sb.Append(string.Format("select * from Weather "));
+
+                    if (!string.IsNullOrEmpty(whereClause))
+                    {
+                        sb.Append("WHERE " + whereClause);
+                    }
+
                     String sql = sb.ToString();
 
                     using (SqlCommand command = new SqlCommand(sql, connection))
@@ -37,7 +64,24 @@ namespace birdCountServer.Controllers
 
                             while (reader.Read())
                             {
-                                retWeather = reader.GetString(0);
+                                var add = new Weather();
+                                add.Id = reader.GetInt32(0);
+                                add.LowTemp = reader.GetDecimal(1);
+                                add.HighTemp = reader.GetDecimal(2);
+                                add.WindDirection = reader.GetString(3);
+                                add.LowWind = reader.GetDecimal(4);
+                                add.HighWind = reader.GetDecimal(5); 
+                                add.SnowMinDepth = reader.GetDecimal(6);
+                                add.SnowMaxDepth = reader.GetDecimal(7);
+                                add.StillWater = reader.GetString(8);
+                                add.MovingWater = reader.GetString(9);
+                                add.AMCloud = reader.GetString(10);
+                                add.PMCloud = reader.GetString(11);
+                                add.AMRain = reader.GetString(12);
+                                add.PMRain = reader.GetString(13);
+                                add.AMSnow = reader.GetString(14);
+                                add.PMSnow = reader.GetString(15);
+                                retWeather.Add(add);
                             }
                         }
                     }
---
+++ birdCountServer/Domain/Weather.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace birdCountServer.Domain
+{
+    public class Weather
+    {
+        public int Id { get; set;  } 
+        public decimal LowTemp { get; set; }
+        public decimal HighTemp { get; set; }
+        public string WindDirection { get; set; }
+        public decimal LowWind { get; set; }
+        public decimal HighWind { get; set; }
+        public decimal SnowMinDepth { get; set; }
+        public decimal SnowMaxDepth { get; set; }
+        public string StillWater { get; set; }
+        public string MovingWater { get; set; }
+        public string AMCloud { get; set; }
+        public string PMCloud { get; set; }
+        public string AMRain { get; set; }
+        public string PMRain { get; set; }
+        public string AMSnow { get; set; }
+        public string PMSnow { get; set; }
+    }
+}
