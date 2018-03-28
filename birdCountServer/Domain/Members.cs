3319d84f7c6da6356efb3cf8a20d26ceaa475195
Sam Green <sam.green@geneva.edu>
2018-03-27 21:24:14 -0400

    Created Team Members API

---
+++ birdCountServer/Domain/Members.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace birdCountServer.Domain
+{
+    public class Members
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+       // public string Method { get; set; }
+        public string HomeAddress { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string TeamID { get; set; }
+    }
+}
