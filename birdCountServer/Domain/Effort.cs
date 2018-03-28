aaa3844daf219167b19575f2cc33b6318ca4ebed
Stephen Maus <stephen_maus@yahoo.com>
2018-03-27 21:12:51 -0400

    Added Effort API

---
+++ birdCountServer/Domain/Effort.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace birdCountServer.Domain
+{
+    public class Effort
+    {
+        public int Id { get; set; }
+        public string CountStart { get; set; }
+        public string CountEnd { get; set; }
+        public double DayMilesDriven { get; set; }
+        public double DayHoursDriven { get; set; }
+        public double DayMilesWalked { get; set; }
+        public double DayHoursWalked { get; set; }
+        public double MilesOwling { get; set; }
+        public double HoursOwling { get; set; }
+        public int TeamId { get; set; }
+    }
+}
